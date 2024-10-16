b0d2ce05ca9ed425aa00a799ec7a4ca73cc8cbcf
florence <loracodes3@gmail.com>
2024-10-16 15:20:22 +0300

    commit

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 function findMatching(drivers, name) {
     return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
   }
+
+  const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Bobby"];
+console.log(findMatching(drivers, "Bobby"));  // It outputs ["Bobby", "Bobby"]
+console.log(findMatching(drivers, "Sammy"));  // It outputs ["Sammy"]
+console.log(findMatching(drivers, "bobby"));  // It outputs ["Bobby", "Bobby"] (case insensitive)
+
   
